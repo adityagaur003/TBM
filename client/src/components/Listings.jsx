3ef1399be870e764a415d559028ee4aa71770ca3
adityagaur003 <adityagirdharigaur@gmail.com>
2024-07-08 06:20:24 +0530

    client fixed

--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -18,8 +18,8 @@ const Listings = () => {
     try {
       const response = await fetch(
         selectedCategory !== "All"
-          ? `http://localhost:3001/properties?category=${selectedCategory}`
-          : "http://localhost:3001/properties",
+          ? `https://tbm-omega.vercel.app/properties?category=${selectedCategory}`
+          : "https://tbm-omega.vercel.app/properties",
         {
           method: "GET",
         }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
             <Person sx={{ color: variables.darkgrey }} />
           ) : (
             <img
-              src={`http://localhost:3001/${user.profileImagePath.replace(
+              src={`https://tbm-omega.vercel.app/${user.profileImagePath.replace(
                 "public",
                 ""
               )}`}
