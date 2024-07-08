2e8e0cc1e7e1d50dff7bc309c7ddb1cbd7c8af76
adityagaur003 <adityagirdharigaur@gmail.com>
2024-07-08 05:46:27 +0530

    added express

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-// const express = require("express");
+const express = require('express'); // Add this line to import express
+
 const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const cors = require("cors");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config(); // Load environment variables from .env file
 
-const authRoutes = require("./routes/auth.js");
-const listingRoutes = require("./routes/listing.js");
-const userRoutes = require("./routes/user.js");
-const bookingRoutes = require("./routes/booking.js");
+const authRoutes = require('./routes/auth.js');
+const listingRoutes = require('./routes/listing.js');
+const userRoutes = require('./routes/user.js');
+const bookingRoutes = require('./routes/booking.js');
 
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Parse JSON bodies
@@ -18,26 +19,28 @@ app.use(express.json()); // Parse JSON bodies
 app.use(express.static('public'));
 
 /* ROUTES */
-app.use("/auth", authRoutes);
-app.use("/properties", listingRoutes);
-app.use("/users", userRoutes);
-app.use("/bookings", bookingRoutes);
+app.use('/auth', authRoutes);
+app.use('/properties', listingRoutes);
+app.use('/users', userRoutes);
+app.use('/bookings', bookingRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 3001; // Default to 3001 if PORT is not specified
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log("Connected to MongoDB");
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(`Error connecting to MongoDB: ${err}`);
   });
-})
-.catch((err) => {
-  console.error(`Error connecting to MongoDB: ${err}`);
-});
 
 module.exports = app; // Export your Express app if needed for testing
+
